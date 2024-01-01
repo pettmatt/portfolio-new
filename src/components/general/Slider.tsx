cdea8181911d9ee879b9d107409c9d054b257d5d
Petteri Mattila <petterimattila@protonmail.com>
2024-01-01 15:19:06 +0200

    Removing unnecessary code

--- src/components/general/Slider.tsx
+++ src/components/general/Slider.tsx
@@ -1,60 +1,58 @@
-"use client"
-import { register } from "swiper/element/bundle"
+// "use client"
+// import { register } from "swiper/element/bundle"
 
-import { project, skill } from "@/types/strapi-components"
-import { fab } from "@fortawesome/free-brands-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { useEffect, useRef } from "react"
+// import { project, skill } from "@/types/strapi-components"
+// import { useEffect, useRef } from "react"
 
-register()
+// register()
 
-export default function Slider({ array }: { array: skill[] | project[] }) {
-    const swiperRef = useRef(null)
+// export default function Slider({ array }: { array: skill[] | project[] }) {
+//     const swiperRef = useRef(null)
     
-    useEffect(() => {
-        if (swiperRef.current !== null) {
-            swiperRef.current.addEventListener("swiperprogress", (e: any) => {
-                const [swiper, progress] = e.details
-                console.log("Progress", progress)
-            })
+//     useEffect(() => {
+//         if (swiperRef.current !== null) {
+//             swiperRef.current.addEventListener("swiperprogress", (e: any) => {
+//                 const [swiper, progress] = e.details
+//                 console.log("Progress", progress)
+//             })
     
-            swiperRef.current.addEventListener("swiperslidechange", (e: any) => {
-                console.log("slide changed")
-            })
-        }
-    }, [])
+//             swiperRef.current.addEventListener("swiperslidechange", (e: any) => {
+//                 console.log("slide changed")
+//             })
+//         }
+//     }, [])
 
-    return (
-        <swiper-container ref={swiperRef}
-            breakpoints={{
-                1250: {
-                    slidesPerView: 6
-                },
-                700: {
-                    slidesPerView: 5
-                },
-                0: {
-                    slidesPerView: 3
-                }
-            }}
-            space-between="3"
-            slides-per-view="5"
-            grid-rows="3"
-            mousewheel-force-to-axis="true"
-        >
-            {
-                array.map((item: skill | project, index: number) => {
-                    console.log("item", item.attributes.name)
-                    return <swiper-slide key={`swiper-item-${index}`}>
-                        {/* <div className="w-max">
-                //             <FontAwesomeIcon icon={fab[item.attributes.class_name || "faJava" ]} size="lg"/>
-                //         </div> */}
-                        <p>
-                            { item.attributes.name }
-                        </p>
-                    </swiper-slide>
-                })
-            }
-        </swiper-container>
-    )
-}
+//     return (
+//         <swiper-container ref={swiperRef}
+//             breakpoints={{
+//                 1250: {
+//                     slidesPerView: 6
+//                 },
+//                 700: {
+//                     slidesPerView: 5
+//                 },
+//                 0: {
+//                     slidesPerView: 3
+//                 }
+//             }}
+//             space-between="3"
+//             slides-per-view="5"
+//             grid-rows="3"
+//             mousewheel-force-to-axis="true"
+//         >
+//             {
+//                 array.map((item: skill | project, index: number) => {
+//                     console.log("item", item.attributes.name)
+//                     return <swiper-slide key={`swiper-item-${index}`}>
+//                         {/* <div className="w-max">
+//                 //             <FontAwesomeIcon icon={fab[item.attributes.class_name || "faJava" ]} size="lg"/>
+//                 //         </div> */}
+//                         <p>
+//                             { item.attributes.name }
+//                         </p>
+//                     </swiper-slide>
+//                 })
+//             }
+//         </swiper-container>
+//     )
+// }
--- src/components/general/Socials.tsx
+++ src/components/general/Socials.tsx
@@ -1,10 +1,12 @@
-export default function Continue({ socials = [] }) {
+import { social } from "@/types/strapi-components"
+
+export default function Continue({ socials = [] }: { socials: social[] }) {
     return (
         <div>
             <h1>My links</h1>
             <ul>
                 { socials.map(social => (
-                    <li key={ social.id }>
+                    <li key={ `social-${social.id}` }>
                         <a href={ social.attributes.link }>
                             { social.attributes.name }
                         </a>
