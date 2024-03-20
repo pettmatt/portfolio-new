062111b6f5b49dd47f85cbc3f6d1da358e5ad186
Petteri Mattila <petterimattila@protonmail.com>
2024-03-20 15:37:36 +0200

    Final styling and push prep tweaks

--- src/services/cmsGetRequest.ts
+++ src/services/cmsGetRequest.ts
@@ -5,7 +5,7 @@ export async function getData(path: string = "", identity: string = "Unnamed") {
         headers: {
             "Authorization": `Bearer ${ process.env.NEXT_PUBLIC_CMS_API_KEY}`
         },
-        cache: "no-cache"
+        cache: "force-cache"
         // Use "no-store" or specify when revalidation should happen to force next/react to not use cached data.
         // cache: "no-store",
         // next: { revalidate: 60 }
